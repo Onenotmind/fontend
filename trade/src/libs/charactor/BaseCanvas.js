65fab2ed9bcb55801b3d05481443bf34ee28b000
chenye01 <chenye01@100tal.com>
2018-04-22 17:57:49 +0800

    add image firsr commit

--- trade/src/libs/charactor/BaseCanvas.js
+++ trade/src/libs/charactor/BaseCanvas.js
@@ -1,15 +1,17 @@
 import testDogEar from '../../images/petbaidu/ear.png'
 import testDogBody from '../../images/petbaidu/body.png'
 import testDogEye from '../../images/petbaidu/eye.png'
+// import CanvasImgTypesArr from './CanvasImgTypes.js'
 // const fs = require('fs')
 // const psth = require('path')
+
 
 class BaseCanvas {
 	constructor (divid) {
 		this.canvasDiv = document.getElementById(divid)
 		this.ctx = this.canvasDiv.getContext('2d')
-		this.width = 400
-		this.height = 400
+		this.width = 200
+		this.height = 200
 		this.canvasDiv.onclick = (e) => {
 			console.log('onclick')
       this.downEvent(e)
@@ -48,10 +50,13 @@ class BaseCanvas {
 		})
 	}
 
-	async drawCharactor () {
-		await this.drawCharactorPart(testDogEar, -10,60)
-		await this.drawCharactorPart(testDogBody, 0, 0)
-		await this.drawCharactorPart(testDogEye, 5, 0)
+	async drawCharactor (CanvasImgTypesArr) {
+		for (let part of CanvasImgTypesArr) {
+			const draw = await this.drawCharactorPart(part, 0, 0)
+		}
+		// await this.drawCharactorPart(testDogEar, -10,60)
+		// await this.drawCharactorPart(testDogBody, 0, 0)
+		// await this.drawCharactorPart(testDogEye, 5, 0)
 	}
 
 	convertCanvasToImageData (canvas) {
@@ -62,8 +67,12 @@ class BaseCanvas {
 		return this.ctx
 	}
 
+	clearCanvas () {
+		this.ctx.clearRect(0, 0, this.width, this.height)
+	}
+
 	// savePng (data, name, cb, errcb) { // 将截图数据存为图片
- //    let dirPath = path.resolve(this.deskPath, './双师未来黑板')
+ //    let dirPath = path.resolve('./', './双师未来黑板')
  //    let exist = fs.existsSync(dirPath)
  //    if (!exist) {
  //      fs.mkdirSync(dirPath)
---
+++ trade/src/libs/charactor/CanvasImgTypes.js
@@ -0,0 +1,32 @@
+import body from '../../images/charactor/body/body.png'
+import belly from '../../images/charactor/belly/belly.png'
+import ear from '../../images/charactor/ear/ear1.png'
+import eye from '../../images/charactor/eye/eye.png'
+import hand from '../../images/charactor/hand/hand.png'
+import head from '../../images/charactor/head/head.png'
+import mouse from '../../images/charactor/mouse/mouse.png'
+import tail from '../../images/charactor/tail/tail.png'
+
+const bodyArr = [body]
+const bellyArr = [belly]
+const earArr = [ear]
+const eyeArr = [eye]
+const handArr = [hand]
+const headArr = [head]
+const mouseArr = [mouse]
+const tailArr = [tail]
+
+const CanvasImgTypes = {
+	body: bodyArr,
+	belly: bellyArr,
+	ear: earArr,
+	eye: eyeArr,
+	hand: handArr,
+	head: headArr,
+	mouse: mouseArr,
+	tail: tailArr
+}
+
+let CanvasImgTypesArr = [body, belly, head, ear, eye, hand, mouse, tail]
+
+export default CanvasImgTypesArr
