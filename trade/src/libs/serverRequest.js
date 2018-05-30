541166e0c10599bfb168a3dd3de0432e18ad96b8
chenye01 <chenye01@100tal.com>
2018-05-30 19:55:44 +0800

    backend ip change

--- trade/src/libs/serverRequest.js
+++ trade/src/libs/serverRequest.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const qs = require('qs')
 axios.defaults.withCredentials = true
 const ssapiAxios = axios.create({
-  baseURL: 'http://47.74.183.93/',
+  baseURL: 'http://47.88.158.254/',
   timeout: 10000
 })
 // axios.defaults.headers.common['token'] = '123'
